def892ccad208abd58c3e7b9267d0fb458481fd8
wojtek3509 <wojtekdalke333@gmail.com>
2025-05-03 12:39:37 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowano jako {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+bot.run(os.getenv("DISCORD_TOKEN"))
